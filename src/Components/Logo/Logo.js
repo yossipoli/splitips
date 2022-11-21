2b740c296702a48a68058abfd109c6ca35acf3cf
yossipoli <yossipoli@gmail.com>
2022-11-21 19:28:21 +0200

    logo resize

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -4,11 +4,6 @@ function Logo() {
     return (
         <div className="logo">
             <img src="./logoTitle.png" alt="TipSpliT" />
-            {/* <h1>
-                Tip
-                <span className="dollar">$</span>
-                pliT
-            </h1> */}
         </div>
     );
 }
