af5f7bbc3fe39ea030a70955e53abaa131e86e6e
yossipoli <yossipoli@gmail.com>
2022-12-03 19:07:50 +0200

    change the server link

--- src/DAL/API.js
+++ src/DAL/API.js
@@ -1,6 +1,6 @@
 
 // eslint-disable-next-line no-unused-vars
-const SERVER = 'https://tipsplit-server.cyclic.app'
+const SERVER = 'https://server2.tipsplit.click/'
 // eslint-disable-next-line no-unused-vars
 const LOCAL = 'http://localhost:4100'
 
