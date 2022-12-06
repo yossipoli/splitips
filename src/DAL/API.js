f1e072434c96b54c1cc2f524cd21934cb8476e03
yossipoli <yossipoli@gmail.com>
2022-12-06 13:53:53 +0200

    server link

--- src/DAL/API.js
+++ src/DAL/API.js
@@ -1,6 +1,6 @@
 
 // eslint-disable-next-line no-unused-vars
-const SERVER = 'http://server2.tipsplit.click'
+const SERVER = 'https://tipsplit-server.herokuapp.com/'
 // eslint-disable-next-line no-unused-vars
 const LOCAL = 'http://localhost:4100'
 
